88675e9de8aa2a59a1a3603c24348d20121d0a4c
alacham <alacham@volny.cz>
2013-03-09 18:18:36 +0100

    add output for collatz

--- collatz.py
+++ collatz.py
@@ -14,3 +14,6 @@ def collatz(num):
 def print_collatz_csv(first_n):
     for i in range(1,first_n):
         print i, ';', ";".join(map(str,collatz(i)))
+
+if __name__ == '__main__':
+    print_collatz_csv(10000)
