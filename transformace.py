1b2e47d17b4f7a9235c3730c6e900ed5de5cae79
alacham <alacham@volny.cz>
2013-04-18 01:30:07 +0200

    triangulace, transformace

---
+++ transformace.py
@@ -0,0 +1,252 @@
+
+import numpy as np
+import copy
+import svgwrite
+from math import degrees, radians, cos, sin
+
+from chaos_game import ngram_vertexes
+
+
+
+def multiply_shift(a, b, howmuch):
+    x = b[0] - a[0]
+    y = b[1] - a[1]
+    return [howmuch * x, howmuch * y]
+
+class Line:    
+    def __init__(self, p1, p2):
+        self.x1, self.y1 = p1
+        self.x2, self.y2 = p2
+    
+    def to_svg(self, svgdwg, offset):
+        dwg = svgdwg
+        dwg.add(dwg.line((self.x1 + offset, self.y1 + offset), (self.x2 + offset, self.y2 + offset), stroke=svgwrite.rgb(10, 10, 16, '%')))
+        
+    def apply_transform(self, matr):
+        p1m = np.mat("%f ; %f; 1" % (self.x1, self.y1))
+        p2m = np.mat("%f ; %f; 1" % (self.x2, self.y2))
+        
+        p1t = matr * p1m
+        p2t = matr * p2m
+        
+        self.x1, self.y1 = p1t[0, 0], p1t[1, 0]
+        self.x2, self.y2 = p2t[0, 0], p2t[1, 0]
+    
+    def length(self):
+        a = (self.x1 - self.x2) ** 2
+        b = (self.y1 - self.y2) ** 2
+        return (a + b) ** 0.5
+    
+    @property
+    def p1(self):
+        return self.x1, self.y1
+    
+    @property
+    def p2(self):
+        return self.x2, self.y2
+
+
+
+class Polygon:
+    def __init__(self, plist):
+        self.lines = []
+        for i in range(len(plist)):
+            self.lines.append(Line(plist[i], plist[(i + 1) % len(plist)]))
+    
+    def to_svg(self, svgdwg, offset):
+        for l in self.lines:
+            l.to_svg(svgdwg, offset)
+    
+    def apply_transform(self, matr):
+        for l in self.lines:
+            l.apply_transform(matr)
+    
+    def points(self):
+        points = []
+        for l in self.lines:
+            points.append(l.p1)
+        return points
+
+
+def shiftMatrix(x=0, y=0):
+    return np.mat("1 0 %f; 0 1 %f; 0 0 1" % (x, y))
+
+def rotationMatrix(degs):
+    rads = radians(degs)
+    return np.mat("%f %f 0; %f %f 0; 0 0 1" % (cos(rads), -sin(rads), sin(rads), cos(rads)))
+
+def scaleMatrix(kx=1, ky=1):
+    return np.mat("%f 0 0; 0 %f 0; 0 0 1" % (kx, ky))
+
+def reflectionMatrix():
+    return np.mat("-1 0 0; 0 1 0; 0 0 1")
+
+def shearMatrix(k):
+    return np.mat("1 %f 0; 0 1 0; 0 0 1" % k)
+
+
+def abcdef2Matrix(a, b, c, d, e, f):
+    return np.mat("%f %f %f; %f %f %f; 0 0 1" % (a, b, e, c, d, f))
+
+
+def composeMatrix(matlist):
+    retmat = None
+    for mat in matlist:
+        if retmat is None:
+            retmat = mat
+        else:
+            retmat = mat * retmat
+    return retmat
+
+
+
+def snail(): #like in slides
+    dwg = svgwrite.Drawing('test.svg')
+    rm = rotationMatrix(20)
+    sm = scaleMatrix(1.1, 1.1)
+    shiftm = shiftMatrix(5, 10)
+    shiftm2 = shiftMatrix(50)
+    
+    am = composeMatrix([rm, sm, shiftm])
+    pol = Polygon([(0, 0), (100, 0), (100, 100), (0, 100)])
+    pol.to_svg(dwg, 500)
+    for i in range(15):
+        pol.apply_transform(am)
+        pol.to_svg(dwg, 500)
+    dwg.save()
+    
+def ufo():#like in slides
+    offset = 120
+    dwg = svgwrite.Drawing('ufo.svg')
+    rm = rotationMatrix(10)
+    sm = scaleMatrix(1.1, 0.8)
+    
+    am = composeMatrix([rm, sm])
+    pol = Polygon([(-50, -50), (50, -50), (50, 50), (-50, 50)])
+    pol.to_svg(dwg, offset)
+    for i in range(15):
+        pol.apply_transform(am)
+        pol.to_svg(dwg, offset)
+    dwg.save()
+
+
+def roadsnail(): #like in slides
+    offset = 100
+    dwg = svgwrite.Drawing('roadsnail.svg')
+    rm = rotationMatrix(-10)
+    sm = scaleMatrix(0.9, 0.9)
+    shiftm = shiftMatrix(50, 50)
+    shearm = shearMatrix(1.3)
+    
+    am = composeMatrix([shearm, rm, sm, shiftm])
+    pol = Polygon([(0, 0), (100, 0), (100, 100), (0, 100)])
+    pol.to_svg(dwg, offset)
+    for i in range(25):
+        pol.apply_transform(am)
+        pol.to_svg(dwg, offset)
+    dwg.save()
+
+
+def recurse_transformations(polygon, canvas, n=10, offset=0):
+    if  n == 0 or polygon.lines[0].length() <= 5 :
+        polygon.to_svg(canvas, offset)
+        return
+
+    vertices = polygon.points()
+    pos = vertices[0]
+    
+    polygon.apply_transform(shiftMatrix(*map(lambda x:-x, pos)))
+    polygon.apply_transform(scaleMatrix(0.5, 0.5))
+    polygon.apply_transform(shiftMatrix(*list(pos)))
+    
+    for v in vertices:
+        poly = copy.deepcopy(polygon)
+        poly.apply_transform(shiftMatrix(*multiply_shift(pos, v, 0.5)))
+        
+        recurse_transformations(poly, canvas, n - 1)
+    
+    return
+    
+    
+
+def recur_triangle():
+    verts = ngram_vertexes(3, 400)
+    triang = Polygon(verts)
+    dwg = svgwrite.Drawing('triang.svg')
+    
+    offset = 0
+    
+    dwg.add(dwg.circle((verts[0][0] + offset, verts[0][1] + offset), r=1, stroke=svgwrite.rgb(255, 0, 0, '%')))
+    dwg.add(dwg.circle((verts[1][0] + offset, verts[1][1] + offset), r=1, stroke=svgwrite.rgb(255, 0, 0, '%')))
+    dwg.add(dwg.circle((verts[2][0] + offset, verts[2][1] + offset), r=1, stroke=svgwrite.rgb(255, 0, 0, '%')))
+        
+    
+    recurse_transformations(triang, dwg, 7, 100)
+    dwg.save()
+
+
+
+def star():
+    dwg = svgwrite.Drawing('star.svg')
+    ssize = 400
+    verts = ngram_vertexes(4, ssize)
+    triang = Polygon(verts)
+    
+    transforms = [abcdef2Matrix(0.255, 0, 0, 0.255, ssize * 0.3726, ssize * 0.6714),
+                  abcdef2Matrix(0.255, 0, 0, 0.255, ssize * 0.1146, ssize * 0.2232),
+                  abcdef2Matrix(0.255, 0, 0, 0.255, ssize * 0.6306, ssize * 0.2232),
+                  abcdef2Matrix(0.370, -0.642, 0.642, 0.370, ssize * 0.6356, ssize * -0.0061)]
+    
+    general_recur(triang, 14, dwg, 0, scaleMatrix(1, 1), transforms)
+    dwg.save()
+    
+def fern():
+    dwg = svgwrite.Drawing('fern.svg')
+    ssize = 200
+    verts = ngram_vertexes(3, ssize)
+    triang = Polygon(verts)
+    
+    transforms = [abcdef2Matrix(0.849, 0.037, -0.037, 0.849, ssize * 0.075, ssize * 0.183),
+                  abcdef2Matrix(0.197, -0.226, 0.226, 0.197, ssize * 0.4, ssize * 0.049),
+                  abcdef2Matrix(-0.15, 0.283, 0.26, 0.237, ssize * 0.575, ssize * 0.084),
+                  abcdef2Matrix(0, 0, 0, 0.16, ssize * 0.5, ssize * 0)]
+    
+    general_recur(triang, 10, dwg, 0, scaleMatrix(1, 1), transforms)
+    general_recur(triang, 0, dwg, 0, scaleMatrix(1, 1), transforms)
+    general_recur(triang, 1, dwg, 0, scaleMatrix(1, 1), transforms)
+#    general_recur(triang, 2, dwg, ssize, scaleMatrix(1,1), transforms)
+#    general_recur(triang, 3, dwg, ssize, scaleMatrix(1,1), transforms)
+    dwg.save()
+    
+
+def general_recur(polyg, howmany, canvas, offset, curtransf, transforms):
+    l1 = copy.copy(polyg.lines[0])
+    l2 = copy.copy(polyg.lines[-1])
+    l1.apply_transform(curtransf)
+    l2.apply_transform(curtransf)
+    ll = l1.length()
+    ll2 = l2.length()
+    
+    if  howmany == 0 or (ll <= 3 or ll2 <= 3):
+#        print howmany
+        if ll > 10 and ll2 > 10:
+            return
+        p = copy.deepcopy(polyg)
+        p.apply_transform(curtransf)
+        p.to_svg(canvas, offset)
+#        print polyg.points()
+        return
+
+#    pos = polyg.points()[0]
+    for t in transforms:
+        tr = t * curtransf
+        general_recur(polyg, howmany - 1, canvas, offset, tr, transforms)
+
+
+if __name__ == '__main__':
+    fern()
+#    star()
+    
+    
+
+
---
+++ triangulation.py
@@ -0,0 +1,136 @@
+import numpy as np
+import copy
+import svgwrite
+from math import degrees, radians, cos, sin
+import random
+
+class Line:    
+    def __init__(self, p1, p2):
+        self.x1, self.y1 = p1
+        self.x2, self.y2 = p2
+    
+    def to_svg(self, svgdwg, offset):
+        dwg = svgdwg
+        dwg.add(dwg.line((self.x1 + offset, self.y1 + offset), (self.x2 + offset, self.y2 + offset), stroke=svgwrite.rgb(10, 10, 16, '%')))
+        
+    def apply_transform(self, matr):
+        p1m = np.mat("%f ; %f; 1" % (self.x1, self.y1))
+        p2m = np.mat("%f ; %f; 1" % (self.x2, self.y2))
+        
+        p1t = matr * p1m
+        p2t = matr * p2m
+        
+        self.x1, self.y1 = p1t[0, 0], p1t[1, 0]
+        self.x2, self.y2 = p2t[0, 0], p2t[1, 0]
+    
+    def length(self):
+        a = (self.x1 - self.x2) ** 2
+        b = (self.y1 - self.y2) ** 2
+        return (a + b) ** 0.5
+    
+    @property
+    def p1(self):
+        return self.x1, self.y1
+    
+    @property
+    def p2(self):
+        return self.x2, self.y2
+
+def lineseg_intersect(l1, l2):
+    d = (l2.y2 - l2.y1) * (l1.x2 - l1.x1) - (l2.x2 - l2.x1) * (l1.y2 - l1.y1)
+    if d == 0: #rovnobezky
+        return False
+    n_a = (l2.x2 - l2.x1) * (l1.y1 - l2.y1) - (l2.y2 - l2.y1) * (l1.x1 - l2.x1)
+    n_b = (l1.x2 - l1.x1) * (l1.y1 - l2.y1) - (l1.y2 - l1.y1) * (l1.x1 - l2.x1)
+    
+    u_a = n_a / float(d)
+    u_b = n_b / float(d)
+    if 0 <= u_a <= 1 and 0 <= u_b <= 1:
+        x = l1.x1 + (u_a * (l1.x2 - l1.x1))
+        y = l1.y1 + (u_a * (l1.y2 - l1.y1))
+        return (x,y)
+    else:#prunik mimo usecky
+        return False
+
+def p_dist(p1, p2):
+        a = (p1[0] - p2[0]) ** 2
+        b = (p1[1] - p2[1]) ** 2
+        return (a + b) ** 0.5
+
+
+def same_point(p1,p2,acc=0.000001):
+    if (p1[0]-acc) <= p2[0] <= (p1[0]-acc) and (p1[1]-acc) <= p2[1] <= (p1[1]-acc):
+        return True
+    return False
+
+def rand_second_point(point, distance):
+    x_dist = random.uniform(-distance, distance)
+    y_dist = random.choice([-1,1]) * (distance**2 - x_dist**2)**0.5
+    return point[0]+x_dist, point[1]+y_dist 
+
+def random_linesegments(ammount,distance, maxi=None):
+    if not maxi:
+        maxi = distance*10
+    
+    linesegments = []
+    for _ in range(ammount):
+        x1 = random.uniform(0, maxi)
+        y1 = random.uniform(0, maxi)
+        
+        p2 = rand_second_point((x1,y1), distance)
+        lineseg = Line((x1,y1),p2)
+        
+        linesegments.append(lineseg)
+    return linesegments
+
+
+def points_from_linesegments(lsegs):
+    points = reduce(lambda x,y: x.add(y.p1) or x.add(y.p2) or x, lsegs, set())
+    return points
+
+
+def triangulate(points):
+#    print points
+    linesegs = set() # lines triangulation consists of
+    usedpoints = set() # processed poitns
+    
+    for p in points:
+#        print "!!!!!!\n",p
+        # add all possible lines which don't break triangulation
+        for p2 in sorted(list(usedpoints)):
+#            print 'p2', p2
+            l = Line(p, p2)
+            nointersects = True
+            for iline in linesegs:
+                if l.p2 == iline.p1 or l.p2 == iline.p2 or \
+                    l.p1 == iline.p1 or l.p1 == iline.p2:
+                    continue
+                inters = lineseg_intersect(l, iline)
+#                print inters
+                if inters:
+                    nointersects = False
+                    break
+            if nointersects:
+#                print 'added'
+                linesegs.add(l)
+        usedpoints.add(p)
+    return linesegs
+
+if __name__ == '__main__':
+    dwg = svgwrite.Drawing('triangulation.svg')
+    offset = 200
+    
+    lsegs = random_linesegments(200, 60)
+    pts = points_from_linesegments(lsegs)
+    
+    pts = list(pts)
+    p0 = pts[0]
+    pts.sort(key=lambda x: p_dist(p0, x))
+    
+    for p in pts:
+        dwg.add(dwg.circle((p[0]+offset,p[1]+offset),r=1, stroke=svgwrite.rgb(255, 0, 0, '%')))
+    lines = triangulate(pts)
+    for l in lines:
+        l.to_svg(dwg,offset)
+    dwg.save()
+    
